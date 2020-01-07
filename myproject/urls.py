8b42b6590418fd0f58f613fde0b65eb896584c75
Gwénolé Paugam <C00254323@itcarlow.ie>
2020-01-07 17:19:52 +0000

    Added new topic and form control

--- myproject/urls.py
+++ myproject/urls.py
@@ -13,14 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-#from django.contrib import admin
-#from django.urls import path
-
-
-#urlpatterns = [
-#    path('admin/', admin.site.urls),
-#]
-
 from django.conf.urls import url
 from django.contrib import admin
 
@@ -28,5 +20,7 @@ from boards import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     url(r'^admin/', admin.site.urls),
 ]
