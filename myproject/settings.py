b7f54b184f376dcf0cac9a6e2d6ba231a86899cd
Gwénolé Paugam <C00254323@itcarlow.ie>
2020-01-08 13:50:52 +0000

    User authentication and password reset added

--- myproject/settings.py
+++ myproject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'widget_tweaks',
 
+    'accounts',
     'boards',
 ]
 
@@ -128,3 +129,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+LOGIN_URL = 'login'
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
